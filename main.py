fdb6630758f274c2013bff1fc1d564a949a6b455
Kolbe Mosher <66530230+FearMyNukes@users.noreply.github.com>
2023-08-15 23:51:51 -0400

    Added replaceBlock
    also removed unnecessary comments

--- main.py
+++ main.py
@@ -2,27 +2,15 @@ from PIL import Image, ImageStat
 from datetime import date
 
 # Object of the image as a whole
-img = Image.open('./photos/beachBall.jpeg')
+img = Image.open('./photos/Scream.jpg')
 # Object used to directly address single pixels in the image
 px = img.load()
-
-# print(img.format, img.size, img.mode)
-
-# img.show()
-
-
-# Convert the image to RGB or 8 bit
-# center of pixel on (0,0) is (0.5,0.5)
-
-# imgStats = ImageStat.Stat(img, mask=None)
-# print(imgStats.mean)
-
 
 # function whose purpose is to give us the result of our edits
 # img is a var of type Image
 def getPhoto(img, name):
     today = date.today()
-    img.save("{}{}.jpeg".format(name, today), "JPEG")
+    img.save("./result/{}{}.jpeg".format(name, today), "JPEG")
 
 def pixelize(img, pixelSize):
     # get dimensions of the photo
@@ -30,6 +18,8 @@ def pixelize(img, pixelSize):
     width = img.size[1]
 
     # define a grid of subsections for the image
+
+
 
 # Finds the average color of a block of pixels
 # block is a Image object instance
@@ -51,15 +41,23 @@ def averageBlock(block):
 
     # Divides the summed RGB values by the total number of pixels 
     # then rounds to two decimals points and returns it in a tuple
-    return ( round((sumR/numPixels), 2), round((sumG/numPixels), 2), round((sumB/numPixels), 2))
+    avg = ( int(sumR/numPixels), int(sumG/numPixels), int((sumB/numPixels)))
+    return avg
 
-averageBlock(img)
+# img:      an Image Object
+# section:  subsection of the total Image
+# position: tuple location of the top left corner where the block was taken from 
+def replaceBlock(img, section, position):
+    blockAvg = averageBlock(section)
+    newBlock = Image.new(mode="RGB", size=section.size, color=blockAvg)
+    img.paste(newBlock, position)
+    return img
+
+# pos = ( int((img.size[0] / 2)-100), int((img.size[1] / 2)-100), int((img.size[0] / 2)+100), int((img.size[1] / 2)+100) )
+# block = img.crop(pos)
+# t1 = replaceBlock(img, block, (pos[0], pos[1]))
+# getPhoto(t1, "test")
+
+# averageBlock(img)
+
 # pixelize(img)
-# box = (100, 100, 400, 400)
-# region = im.crop(box)
-# code to create a crop of the photo
-
-
-
-# region = region.transpose(Image.Transpose.ROTATE_180)
-# im.paste(region, box)
