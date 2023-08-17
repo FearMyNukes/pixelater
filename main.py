3de0a7f31d1c31261f81afc1cdc4e9ccc97b5d1e
Kolbe Mosher <66530230+FearMyNukes@users.noreply.github.com>
2023-08-17 17:27:58 -0400

    added code to create a gif

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ from PIL import Image
 from datetime import date
 
 # intialize Image object
-IMG = Image.open('./photos/Scream.jpg')
+IMG = Image.open('./photos/Scream.png')
 
 # Saves photo to filesystem
 def getPhoto(img, name):
     today = date.today()
-    img.save("./result/{}_{}.jpeg".format(name, today), "JPEG")
+    img.save("./result/{}_{}.png".format(name, today), "PNG")
 
 ### Finds the average color of a block of pixels ###
 
@@ -37,11 +37,11 @@ def averageBlock(block):
 # of pixels then replace the block in the original image ###
 
 # img:      an Image Object
-# section:  subsection of the total Image
+# block:  subsection of the total Image
 # position: tuple location of the top left corner where the block was taken from 
 def replaceBlock(img, block, position):
     blockAvg = averageBlock(block)
-    newBlock = Image.new(mode="RGB", size=block.size, color=blockAvg)
+    newBlock = Image.new(mode="RGBA", size=block.size, color=blockAvg)
     img.paste(newBlock, position)
     return img
 
@@ -51,7 +51,7 @@ def replaceBlock(img, block, position):
 # img:       an Image Object
 # name:      name you want to associate with the file when save will also save with the date
 # pixelSize: length of each size of the square to be averaged
-def pixelize(img, name, pixelSize):
+def pixelize(img, pixelSize):
     # get dimensions of the photo
     height = img.size[1]
     width = img.size[0]
@@ -67,6 +67,18 @@ def pixelize(img, name, pixelSize):
             x += pixelSize
         x = 0
         y += pixelSize
-    getPhoto(img, name)
+    return img
 
-pixelize(IMG, "scream", 50)
+# pixelize(IMG, "scream", 50)
+gifIMGS = [IMG]
+i = 5
+while (i < 200):
+    gifIMGS.append( pixelize(IMG, i) )
+    newimg = pixelize(IMG, i)
+    # getPhoto(newimg, i)
+    i += 50
+
+# print(gifIMGS)
+print(len(gifIMGS))
+print(gifIMGS[1:])
+gifIMGS[0].save('Scream.gif', save_all=True, append_images=gifIMGS[1:], optimize=False, duration=250, loop=0)
